6febdf06468c902a5e627aaf490ace76b9ad775e
tsoushi <sosimds@gmail.com>
2022-07-28 22:55:58 +0900

    utilityの仕様変更に伴い、utilityの呼び出し箇所を修正

--- linebot/event/message/text.js
+++ linebot/event/message/text.js
@@ -1,4 +1,4 @@
-import * as utility from "../../../utility.js"
+import { utility } from "../../../utility.js"
 
 export const textEvent = async (event, client) => {
     let message;
@@ -6,18 +6,18 @@ export const textEvent = async (event, client) => {
     console.log('テキストメッセージを受信: ' + event.message.text);
     //メッセージのテキストごとに条件分岐
     if(event.message.text.startsWith('deviceid=')) {
-        utility.addDeviceID(event.userId, event.message.text.substr(9));
+        utility.database.addDeviceID(event.userId, event.message.text.substr(9));
         return;
     }
 
     if (event.message.text.startsWith('removedeviceid=')) {
-        utility.removeDeviceID(event.message.text.substr(15));
+        utility.database.removeDeviceID(event.message.text.substr(15));
         return;
     }
     
     switch (event.message.text) {
         default: {
-            message = utility.makeTextMessage('このメッセージの返信には対応していません...');
+            message = utility.makeMessage.text('このメッセージの返信には対応していません...');
             break;
         }
     }
