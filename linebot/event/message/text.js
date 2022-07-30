81ddf681073506e61a97f646152dff769e3eb4f0
tsoushi <sosimds@gmail.com>
2022-07-30 14:36:32 +0900

    text.jsを大幅改修。デバイスIDの登録確認を追加。

--- linebot/event/message/text.js
+++ linebot/event/message/text.js
@@ -1,37 +1,40 @@
 import { utility } from "../../../utility.js"
 
-export const textEvent = async (event) => {
-    let message;
+const nullDeviceIdMessage = utility.makeMessage.text('デバイスIDを登録してください');
 
-    console.log('テキストメッセージを受信: ' + event.message.text);
+// デバイスIDが登録されているか確認する。
+// されていなければreplyMessageを送り、戻り値としてfalseを返す。
+const isDeviceIdRegistered = async (event) => {
+    if (await utility.database.getDeviceIDFromUserID(event.source.userId) === null) {
+        return false;
+    } else {
+        return true;
+    }
+}
+
+export const textEvent = async (event) => {
+    const text = event.message.text;
+    const userId = event.source.userId;
+
+    console.log('テキストメッセージを受信: ' + text);
+    
     //メッセージのテキストごとに条件分岐
-    if(event.message.text.startsWith('deviceid=')) {
-        utility.database.addDeviceID(event.source.userId, event.message.text.substr(9));
-        return;
+    if(text.startsWith('deviceid=')) {
+        utility.database.addDeviceID(userId, text.substr(9));
+        return utility.makeMessage.text(`デバイスID「${text.substr(9)}」を登録しました`);
     }
 
-    if (event.message.text == 'removedeviceid') {
-        utility.database.removeDeviceIDByUserID(event.source.userId);
-        return;
+    if (text == 'removedeviceid') {
+        if (!await isDeviceIdRegistered(event)) return nullDeviceIdMessage; // デバイスIDが存在するかどうかを調べる
+        utility.database.removeDeviceIDByUserID(userId);
+        return utility.makeMessage.text('デバイスIDの登録を解除しました');
     }
     
-    if (event.message.text.startsWith('reply=')) {
-        utility.database.addReplyMessageByUserId(event.source.userId, event.message.text.substr(6))
-            .catch(() => {
-                utility.lineClient.replyMessage(
-                    event.replyToken,
-                    utility.makeMessage.text('デバイスIDが登録されていません')
-                );
-            });
-        return;
+    if (text.startsWith('reply=')) {
+        if (!await isDeviceIdRegistered(event)) return nullDeviceIdMessage;
+        utility.database.addReplyMessageByUserId(userId, text.substr(6));
+        return utility.makeMessage.text('返信メッセージを送信しました！');
     }
 
-    switch (event.message.text) {
-        default: {
-            message = utility.makeMessage.text('このメッセージの返信には対応していません...');
-            break;
-        }
-    }
-
-    return message;
+    return;
 }
