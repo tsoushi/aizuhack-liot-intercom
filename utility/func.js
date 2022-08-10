1677865f054a3ad38edbada220834d0dd5f8634f
tsoushi <sosimds@gmail.com>
2022-08-10 21:16:12 +0900

    0埋めするようにした

--- utility/func.js
+++ utility/func.js
@@ -30,7 +30,7 @@ export const uploadFromMemory = async (destFileName, content) => {
 }
 
 export const dateToDatabaseDate = (date) => {
-  return `${date.getFullYear()}-${date.getMonth()+1}-${date.getDate()} ${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}`;
+  return `${date.getFullYear()}-${String(date.getMonth()+1).padStart(2, '0')}-${String(date.getDate()).padStart(2, '0')} ${String(date.getHours()).padStart(2, '0')}:${String(date.getMinutes()).padStart(2, '0')}:${String(date.getSeconds()).padStart(2, '0')}`;
 }
 
 export const dateToLocaleString = (date) => {
